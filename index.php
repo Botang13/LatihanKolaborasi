93366154bc4a9cab963f209f50b5aa0d509e5f49
rhesanic <rhesanicop@gmail.com>
2019-03-29 14:57:27 +0700

    file index.html

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "<h2> Selamat berlatih menjadi kolaborator</h2>";
 echo "<h3> Isi Nama dan NIM Anda dibawah baris kode ini</h3>";
 <<<<<<<HEAD
-echo "<h3>Herbowo Prasetyo (155410070)</h3>";
+echo "<h3>Rhesa Nico Philaret (165610095)</h3>";
 =======
 >>>>>>> upstream/master
 ?>
