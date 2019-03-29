3aa0c62c8704bab58ce68edfc3694c7e1d6c4277
azizahkurnia <mbakaziz25@gmail.com>
2019-03-29 14:30:11 +0700

    file index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+echo "<h2> Selamat berlatih menjadi kolaborator</h2>";
+echo "<h3> Isi Nama dan NIM Anda dibawah baris kode ini</h3>";
+<<<<<<<HEAD
+echo "<h3>Herbowo Prasetyo (155410070)</h3>";
+=======
+>>>>>>> upstream/master
+?>
